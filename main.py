9eb63eb12e0d7a29b6c879050aa09ddf903c5e06
fusionguy100 <jnewell1820@gmail.com>
2025-05-14 19:51:22 -0700

    Created screen and game

---
+++ main.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+import pandas
+import turtle
+
+data = pandas.read_csv("50_states.csv")
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+
+states = data["state"].tolist()
+turtle.shape(image)
+
+
+
+current_guesses = []
+current_answers = 0
+
+
+
+while True:
+    answer_state = screen.textinput(title=f"{current_answers}/50 States Correct", prompt="What's another state name?").title()
+
+    if answer_state == "Exit":
+        break
+
+    if answer_state in states and answer_state not in current_guesses:
+        state_data = data[data.state == answer_state]
+        x = int(state_data.x)
+        y = int(state_data.y)
+        writer = Turtle()
+        writer.hideturtle()
+        writer.penup()
+        writer.goto(x,y)
+        writer.write(answer_state, align = "Center", font = ("Arial",8,"normal"))
+        current_guesses.append(answer_state)
+        current_answers += 1
+
+
+    if current_answers == 50:
+        break
+
+
+
